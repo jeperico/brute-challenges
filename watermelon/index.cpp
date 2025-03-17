7351db16ccaba986c235a5f91af1842273153879
João Périco <joaoeperico@gmail.com>
2025-03-17 13:45:03 -0300

    [codeforces] a - watermelon

---
+++ watermelon/index.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+
+int main(){
+  ios_base::sync_with_stdio(0);cin.tie(0);
+	int w;
+	cin >> w;
+	if(w % 2 == 0 && w != 2) {
+		cout << "YES" << endl;
+	} else {
+		cout << "NO" << endl;
+	}
+
+    return 0;
+}
