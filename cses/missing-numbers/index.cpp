1e0d7ee59c6d26559cc12c4f9a73ff4a4de95146
jeperico <joaoeperico@gmail.com>
2025-03-18 18:51:48 -0300

    [cses] missing-numbers

--- cses/missing-numbers/index.cpp
+++ cses/missing-numbers/index.cpp
@@ -1,15 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int ll
 #define endl '\n'
 
 
 void solve() {
   int n;
   cin >> n;
+  vector<int> arr(n);
 
+  int sum = 0;
+  for(int i = 0; i < n - 1; i++) {
+    cin >> arr[i];
+    sum += i + 1;
+  }
+  sum += n;
 
+  for(int el: arr) {
+    sum -= el;
+  }
 
+  cout << sum << endl;
 }
 
 signed main() {
