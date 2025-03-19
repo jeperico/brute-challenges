25454154457b9ae6e863073d791c0d5c2ba53a0e
jeperico <joaoeperico@gmail.com>
2025-03-19 20:39:46 -0300

    [cses] permutations

--- cses/permutations/index.cpp
+++ cses/permutations/index.cpp
@@ -6,40 +6,19 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-
   vector<int> arr(n);
-  for(int i = 0; i < n; i++) {
-    arr[i] = i + 1;
-  }
 
-
-  if (n == 1) cout << 1;
-  else if (n < 4) cout << "NO SOLUTION";
+  
+  if (n == 2 || n == 3) cout << "NO SOLUTION";
   else {
-    int qtd5 = n % 4;
-    int qtd4 = (n / 4) - qtd5;
-    int act = 0;
-
-    while (qtd5 > 0) {
-      int el = arr[act + 4];
-      arr[act + 4] = arr[act + 0];
-      arr[act + 0] = arr[act + 3];
-      arr[act + 3] = arr[act + 2];
-      arr[act + 2] = el;
-
-      qtd5 -= 1;
-      act += 5;
+    int el = 0;
+    for(int i = 1; i <= n; i ++) if(i % 2 == 0) {
+      arr[el] = i;
+      el += 1;
     }
-
-    while (qtd4 > 0) {
-      int el = arr[act + 2];
-      arr[act + 2] = arr[act + 0];
-      arr[act + 0] = arr[act + 1];
-      arr[act + 1] = arr[act + 3];
-      arr[act + 3] = el;
-
-      qtd4 -= 1;
-      act += 4;
+    for(int i = 1; i <= n; i ++) if(i % 2 != 0) {
+      arr[el] = i;
+      el += 1;
     }
 
     for(int i = 0; i < n; i++) {
