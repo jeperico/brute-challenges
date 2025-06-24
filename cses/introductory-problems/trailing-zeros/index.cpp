9f7a295d9445c3611f69b286d06dbd72fc6aed4f
João Périco <joaoeperico@gmail.com>
2025-06-24 14:33:40 +0000

    [cses] trailing zeros start

---
+++ cses/introductory-problems/trailing-zeros/index.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n';
+typedef long long ll;
+
+
+ll power(ll base, ll expo, ll MOD = 0) {
+  ll res = 1;
+
+  while(expo) {
+    if(expo & 1LL) {
+      res = MOD ? (res * base) % MOD : (res * base);
+    }
+    base = MOD ? (base * base) % MOD : (base * base);
+    expo >>= 1LL;
+  }
+
+  return res;
+}
+
+ll factorial(ll number) {
+  if(!number) return 1;
+
+  ll res = 1;
+  for(int i = 1; i <= number; i++) {
+    res *= i;
+  }
+
+  return res;
+}
+
+
+void solve() {
+  ll n = 0;
+  cin >> n;
+  n = factorial(n);
+
+  int count = 0;
+  ll i = 10;
+  while(true) {
+    if (n % i) break;
+
+    count++;
+    i *= 10;
+  }
+  cout << count << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);cin.tie(0);
+  solve();
+  return 0;
+}
