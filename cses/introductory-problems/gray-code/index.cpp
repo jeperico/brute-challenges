b0f0bac0e75e3a800a3d712039cbcc912f2b5e3d
João Périco <joaoeperico@gmail.com>
2025-07-11 04:41:03 +0000

    [feat] clone template

---
+++ cses/introductory-problems/gray-code/index.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n';
+typedef long long ll;
+
+
+ll power(ll base, ll expo, ll MOD = 0) {
+  ll res = 1;
+  while(expo) {
+    if(expo & 1LL) {
+      res = MOD ? (res * base) % MOD : (res * base);
+    }
+    base = MOD ? (base * base) % MOD : (base * base);
+    expo >>= 1LL;
+  }
+  return res;
+}
+
+ll factorial(ll number, ll MOD = 0) {
+  ll res = 1;
+  for(int i = 2; i <= number; i++) {
+    res = MOD ? res * i % MOD : res * i;
+  }
+  return res;
+}
+
+
+void solve() {
+
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);cin.tie(0);
+  int TC = 1;
+  if(TC) {
+    cin >> TC;
+    while(TC--) solve();
+  } else solve();
+  return 0;
+}
