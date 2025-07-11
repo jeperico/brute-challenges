f3ef158a820fb0c8833d751b37c61d4d5788745c
João Périco <joaoeperico@gmail.com>
2025-07-11 04:16:20 +0000

    [feat] palindrome reorder v1

--- cses/introductory-problems/palindrome-reorder/index.cpp
+++ cses/introductory-problems/palindrome-reorder/index.cpp
@@ -26,7 +26,42 @@ ll factorial(ll number, ll MOD = 0) {
 
 
 void solve() {
-  
+  // string word = "JJKKJSTFFKJST";
+  string word;
+  cin >> word;
+  int n = word.length();
+
+  // cout << "[" << word << "]" << endl;
+  int single = 0;
+  for(int i = 0; i < n / 2; i++) {
+    // cout << "i - " << word[i] << endl;
+    for(int j = i + 1; j < n - i; j++) {
+      // cout << word[j] << endl;
+      int last = n - i - 1;
+      if(word[i] == word[j]) {
+        // cout << "CHANGED" << endl;
+        char temp = word[last];
+        word[last] = word[j];
+        word[j] = temp;
+        break;
+      }
+      if(j == last) {
+        single++;
+        int half = n / 2;
+        char temp = word[half];
+        word[half] = word[i];
+        word[i] = temp;
+        i--;
+        // cout << "NO = " << word << endl;
+        if(single == 2 || single == 1 && n % 2 == 0) {
+          cout << "NO SOLUTION" << endl;
+          return;
+        }
+        break;
+      }
+    }
+  }
+  cout << word << endl;
 }
 
 int main() {
